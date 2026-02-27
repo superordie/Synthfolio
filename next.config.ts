ef3929b73619a5699d121c756607a6189d36af77
superordie <superordie@yahoo.com>
2026-02-26 17:50:27 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* This ensures /admin is always treated as /admin/ which 
      helps Firebase App Hosting find your folder correctly. */
   trailingSlash: true, 
